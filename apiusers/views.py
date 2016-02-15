f3d7cbf473b2719906bc56fdbe15e1c72ea98c9e
kostya <ermine.kostya@gmail.com>
2016-02-15 16:37:14 +0200

    add authentification with socialnets

--- apiusers/views.py
+++ apiusers/views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from apiusers.serializers import UserSerializer, GroupSerializer
+from rest_framework import permissions
 
 
 class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
+    # auth
+    permission_classes = (permissions.IsAuthenticated,)
+
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
 
@@ -16,5 +20,8 @@ class GroupViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows groups to be viewed or edited.
     """
+    # auth
+    permission_classes = (permissions.IsAuthenticated,)
+    
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
