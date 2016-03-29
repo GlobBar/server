58d4c7ccc8aef147776d67d0b6d0eae3597bf43a
kostya <ermine.kostya@gmail.com>
2016-03-29 17:09:22 +0300

    add app and methods for push notifications

---
+++ notification/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import permissions
+
+
+class NotificationList(APIView):
+
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request, format=None):
+
+        return Response({'data': 'Successfully add token'})
+
+    def delete(self, request, format=None):
+
+        return Response({'data': 'Successfully delete token'})
