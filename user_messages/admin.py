24fbac8584feaeb724f266b19e681860948db22d
kostya <ermine.kostya@gmail.com>
2016-04-18 18:21:20 +0300

    fix news aded edit button

--- user_messages/admin.py
+++ user_messages/admin.py
@@ -3,7 +3,7 @@ from user_messages.models import NewsMessages
 
 
 class NewsMessagesAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'title', 'body', 'created')
+    list_display = ('pk', 'title', 'body', 'created', 'edit_field')
     list_filter = []
 
     fieldsets = [
@@ -11,9 +11,17 @@ class NewsMessagesAdmin(admin.ModelAdmin):
             'fields': [
                 'title',
                 'body',
-                # 'user_to',
-                # 'created',
             ]})
     ]
+
+    # Actions
+    def edit_field(self, obj):
+        res = '<span style="padding:5px;"><a href="/admin/user_messages/newsmessages/' + str(
+            obj.pk) + '/change/"><span class="changelink">Edit</span></a></span>'
+        return res
+
+    edit_field.allow_tags = True
+    edit_field.short_description = 'ACTIONS'
+
 
 admin.site.register(NewsMessages, NewsMessagesAdmin)
