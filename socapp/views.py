cacf34ee2752eb9b50a5bd7d9e5698c83b4ad924
kostya <ermine.kostya@gmail.com>
2016-04-19 18:38:34 +0300

    add text and logo into fb post2

--- socapp/views.py
+++ socapp/views.py
@@ -34,7 +34,7 @@ class FbPosts(APIView):
                 'message': 'Nightlife',
                 'access_token': user_fb_access_token.token,
                 'description': "Wondering what's going on tonight? Check out the GlobBar app! We help each other out and earn points towards discounted drinks",
-                'link': 'https://itunes.apple.com/en/app/facebook/id284882215?mt=8',
+                'link': 'https://itunes.apple.com',
                 'picture': logo
                 }
             res = requests.post(final_url, data=params)
