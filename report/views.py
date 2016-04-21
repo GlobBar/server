1115bc3a5aef5ebb51f3ac4a4a4506342ff5e55c
kostya <ermine.kostya@gmail.com>
2016-04-21 17:30:21 +0300

    minor fix2

--- report/views.py
+++ report/views.py
@@ -31,7 +31,7 @@ class ReportList(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def post(self, request,  format=None):
+    def delete(self, request,  format=None):
         pk = request.GET.get('report_pk')
         report = self.get_object(pk)
         # if is video /image
