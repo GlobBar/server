114cd465649d3fd2df614c439316e266f9acafa3
Polyakov Kostya <kostya.p.dev@gmail.com>
2019-05-25 13:33:33 +0300

    task-14: migrations

--- apiusers/migrations/0003_profile_place.py
+++ apiusers/migrations/0003_profile_place.py
@@ -9,7 +9,6 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('places', '0027_auto_20190525_0928'),
         ('apiusers', '0002_auto_20190509_1737'),
     ]
 
